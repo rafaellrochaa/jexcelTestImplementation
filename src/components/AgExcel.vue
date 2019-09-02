d850579a7a73fb1322d277f3f34ac6631bc78328
Henrique <henriquevc93@gmail.com>
2019-09-02 18:09:55 -0300

    importar um arquivo externo

--- src/components/AgExcel.vue
+++ src/components/AgExcel.vue
@@ -1,13 +1,16 @@
 <template>
   <div ref="displayExcel">
-    <!-- <div><input type="button" value="Add new row" @click="() => displayExcel.insertRow()" /></div> -->
-    <button @click="valores"> Exibir Valores Selecionados (console) </button>
+    <div class="mb-md">
+      <input type="file" @change="carregar">
+      <button @click="valores">Exibir Valores Selecionados (console)</button>
+    </div>
   </div>
 </template>
 
 <script>
 import jexcel from 'jexcel'
 import 'jexcel/dist/jexcel.css'
+import XLSX from 'xlsx'
 
 export default {
   data () {
@@ -16,7 +19,6 @@ export default {
       posicaoSelecaoInicial: {linha: -1, coluna: -1},
       posicaoSelecaoFinal: {linha: -1, coluna: -1},
       options: {
-        data: [[]],
         minDimensions: [25,30],
         onselection: this.selecaoAtiva
       }
@@ -26,7 +28,41 @@ export default {
     this.displayExcel = jexcel(this.$refs["displayExcel"], this.options)
   },
   methods: {
-    selecaoAtiva (instance, x1, y1, x2, y2, origin) {
+    carregar (fileSelected) {
+      let file = fileSelected.target.files[0]
+      let reader = new FileReader()
+      let name = file.name
+      reader.onload = e => {
+        let results,
+          data = e.target.result,
+          fixedData = this.fixData(data),
+          workbook = XLSX.read(btoa(fixedData), {type: 'base64'}),
+          firstSheetName = workbook.SheetNames[0],
+          worksheet = workbook.Sheets[firstSheetName];
+        let result222 = this.workbook_to_json(workbook)
+        console.log(result222)
+        this.options.data = XLSX.utils.sheet_to_json(worksheet, {header: 1, raw: false})
+        this.displayExcel = jexcel(this.$refs["displayExcel"], this.options)
+      }
+      reader.readAsArrayBuffer(file)
+    },
+    fixData (data) {
+      var o = "", l = 0, w = 10240
+      for(; l<data.byteLength/w; ++l) o+=String.fromCharCode.apply(null,new Uint8Array(data.slice(l*w,l*w+w)))
+      o += String.fromCharCode.apply(null, new Uint8Array(data.slice(l*w)))
+      return o
+    },
+    workbook_to_json (workbook) {
+      let result = {}
+      workbook.SheetNames.forEach(sheetName => {
+        let roa = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheetName], {header: 1})
+        if (roa.length > 0) {
+          result[sheetName] = roa
+        }
+      })
+      return result
+    },
+    selecaoAtiva (instance, x1, y1, x2, y2) {
       this.posicaoSelecaoInicial.linha = y1
       this.posicaoSelecaoInicial.coluna = x1
       this.posicaoSelecaoFinal.linha = y2
@@ -34,19 +70,17 @@ export default {
     },
     valores () {
       let table = this.displayExcel.getData(false)
-      for (let linha = this.posicaoSelecaoInicial.linha; linha < table.length; linha++) {
-        if (linha <= this.posicaoSelecaoFinal.linha) {
-          for (let coluna = 0; coluna < table[linha].length; coluna++) {
-            if (this.posicaoSelecaoInicial.coluna === this.posicaoSelecaoFinal.coluna) {
-              if (coluna === this.posicaoSelecaoInicial.coluna && table[linha][coluna] !== '') {
-                console.log('célula:', jexcel.getColumnNameFromId([coluna, linha]), 'valor:', table[linha][coluna])
-              }
+      for (let linha = this.posicaoSelecaoInicial.linha; linha <= this.posicaoSelecaoFinal.linha; linha++) {
+        for (let coluna = this.posicaoSelecaoInicial.coluna; coluna <= this.posicaoSelecaoFinal.coluna; coluna++) {
+          if (this.posicaoSelecaoInicial.coluna === this.posicaoSelecaoFinal.coluna) {
+            if (coluna === this.posicaoSelecaoInicial.coluna && table[linha][coluna] !== '') {
+              console.log('célula:', jexcel.getColumnNameFromId([coluna, linha]), 'valor: ', table[linha][coluna])
             }
-            else { 
-              if (coluna <= this.posicaoSelecaoFinal.coluna) {
-                if (table[linha][coluna] !== '') {
-                  console.log('célula:', jexcel.getColumnNameFromId([coluna, linha]), 'valor:', table[linha][coluna])
-                }
+          }
+          else {
+            if (coluna <= this.posicaoSelecaoFinal.coluna) {
+              if (table[linha][coluna] !== '') {
+                console.log('célula: ' + jexcel.getColumnNameFromId([coluna, linha]) + ' valor: ' + table[linha][coluna])
               }
             }
           }
@@ -56,3 +90,8 @@ export default {
   }
 }
 </script>
+<style>
+.mb-md {
+  margin-bottom: 8px;
+}
+</style>
